3bfad4dc2046eb8b790f109de82a13c4f65d0006
thejoeman23 <epicjonathan24@gmail.com>
2025-03-20 07:41:09 -0400

    Moving Platform
    - Platform moves
    [DOESNT MOVE THE PLAYER WITH IT PLEASE FIX]

---
+++ Assets/Scripts/PrefabScripts/MovingPlatform.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] float speed = 5;
+    Rigidbody2D rb;
+    bool isMovingRight = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + Vector2.right * speed * Time.fixedDeltaTime * (isMovingRight ? 1 : -1));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+
+    // Wall Detector Trigger (on child object)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player")) // Detects walls or barriers
+        {
+            isMovingRight = !isMovingRight;
+        }
+    }
+}
